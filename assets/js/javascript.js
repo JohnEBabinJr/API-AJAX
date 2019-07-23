83c04f435f46bf8ceea8a9272417dfc4e92d8e9a
John Babin <johnebabinjr@gmail.com>
2019-07-23 15:56:01 -0700

    finised all logic, just need to edit style

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,43 +1,65 @@
-animals=["dog","cat","bird","monkey","fish","bear",]
-
-
-
-
-
+animals = ["dog", "cat", "bird", "monkey", "fish", "bear",]
 
 function renderButtons() {
-
-  // Deleting the movies prior to adding new movies
-  // (this is necessary otherwise we will have repeat buttons)
   $("#buttons-view").empty();
-
-  // Looping through the array of movies
   for (var i = 0; i < animals.length; i++) {
-
-    // Then dynamicaly generating buttons for each movie in the array
-    // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
     var a = $("<button>");
-    // Adding a class
     a.addClass("animals");
-    // Added a data-attribute
     a.attr("data-name", animals[i]);
-    // Provided the initial button text
     a.text(animals[i]);
-    // Added the button to the HTML
     $("#buttons-view").append(a);
   }
 }
+
+function displayGifs() {
+  var animaltype = $(this).attr("data-name");
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animaltype + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    .then(function (response) {
+      Response=response.data;
+      redndergifs();
+    });
+}
+
+function redndergifs() {
+  for (var i = 0; i < Response.length; i++) {
+    var Div = $("<div>");
+    var rating = Response[i].rating;
+    var p = $("<p>").text("Rating: " + rating);
+    var animalImage = $("<img>");
+    animalImage.attr("src", Response[i].images.fixed_height_still.url);
+    animalImage.attr("data-still", Response[i].images.fixed_height_still.url);
+    animalImage.attr("data-animate", Response[i].images.fixed_height.url);
+    animalImage.attr("data-state", "still");
+    animalImage.attr("class", "still");
+    Div.addClass("gif");
+    Div.prepend(p);
+    Div.prepend(animalImage);
+    $("#gif-display").prepend(Div);
+  }
+}
+
 renderButtons();
-// This function handles events where one button is clicked
-$("#add-animal").on("click", function(event) {
+
+$("#add-animal").on("click", function (event) {
   event.preventDefault();
-
-  // This line grabs the input from the textbox
   var animal = $("#animal-input").val().trim();
-
-  // The movie from the textbox is then added to our array
   animals.push(animal);
-
-  // Calling renderButtons which handles the processing of our movie array
   renderButtons();
 });
+
+$(document).on("click", ".still", function (event) {
+  var state = $(this).attr("data-state");
+  if (state == "still") {
+    $(this).attr("src", $(this).attr("data-animate"));
+    $(this).attr("data-state", "animate");
+  } else {
+    $(this).attr("src", $(this).attr("data-still"));
+    $(this).attr("data-state", "still");
+  }
+});
+
+$(document).on("click", ".animals", displayGifs);
